71427bd27ad7bdc32a5bb9c82ecb9365ae841c47
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2023-04-06 23:29:51 -0400

    Initialized the project structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <>
+        <header>
+            <div id="main-title">
+                <div id="title">
+                    नमस्ते 🙏🏻
+                </div>
+                <div id="introduction">
+                    Hello, my name is Dev Chaudhary, and I am gonna be "King of the Pirates" 👑
+                </div>
+            </div>
+            <div id="profile-pic">
+                <img src="./" alt="" />
+            </div>
+        </header>
+    </>
+  )
+}
