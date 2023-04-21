897bfc4a0bc8548dacafd35e6c4d0194d2b1f7e8
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2023-04-21 14:46:43 -0400

    still need to implement quote component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header"
+import Quote from "../components/Quote"
 
 export default function Home() {
   return (
     <>
         <Header />
+        <Quote />
     </>
   )
 }
