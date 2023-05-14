fd412ccc4172c01f9bc2bd5c1313a1e1d03f0d96
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2023-05-13 20:57:42 -0400

    Modulized the Card Data to seperate it into an array

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,18 +7,25 @@ export default function Container() {
     return (
         <div className={styles.card_container}>
             <a href="#"><Image className={styles.arrows} src="/imgs/arrow-left.png" alt="This is just a left arrow" width={40} height={40} /></a>
-            <CardContainer  height={400} width={500} backgroundColor={"bisque"} title={"This is just a testing title"} emoji={"🚀"} text={"This is just a sample text to check whether the card is working or not and let's be honest it is not working for god damn reason I don't know why it isn't working!"} />
-
-            <CardContainer height={200} width={300} backgroundColor={"yellow"} title={"This is just a testing title"} emoji={"🚀"} text={"This is just a sample text to check whether the card is working or not and let's be honest it is not working for god damn reason I don't know why it isn't working!"} />
+            {
+                cards.map((card: Card) => (
+                    <CardContainer key={`${card.title}-${card.emoji}`}
+                        backgroundColor={card.backgroundColor}
+                        title={card.title}
+                        emoji={card.emoji}
+                        text={card.text}
+                    />
+                ))
+            }
 
             <a href="#" ><Image className={styles.arrows} src="/imgs/arrow-right.png" alt="This is just a right arrow" width={40} height={40} /></a>
         </div>
     );
 }
 
-function CardContainer({height, width, backgroundColor, title, emoji, text}: {height: number, width: number, backgroundColor: string, title: string, emoji: string, text: string}) {
+function CardContainer({ backgroundColor, title, emoji, text}: {backgroundColor: string, title: string, emoji: string, text: string}) {
     return(
-        <div className={styles.card} style={{ height, width, backgroundColor }}>
+        <div className={styles.card} style={{ backgroundColor }}>
             <h2 className={styles.card_title}>{title} {emoji}</h2>
             <p className={styles.card_text}>{text}</p>
         </div>
