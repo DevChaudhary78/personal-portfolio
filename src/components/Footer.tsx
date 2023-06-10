fc3e0859cccd45add93ac75e71d778719c5add69
Dev Chaudhary <devchaudhary@Devs-Air.localdomain>
2023-06-09 22:29:19 -0400

    Fixed the footer positioning & removed discord

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
                 <a href="https://twitter.com/devchaudhary78"><Image className={styles.icon} src="/icons/twitter.svg" alt="Twitter" height={40} width={40} /></a>
                 <a href="https://www.instagram.com/devchaudhary.78/?next=%2F"><Image className={styles.icon} src="/icons/instagram.svg" alt="Instagram" height={40} width={40} /></a>
                 <a href="https://github.com/DevChaudhary78/personal-portfolio"><Image className={styles.icon} src="/icons/code.png" alt="Code" height={40} width={40} /></a>
-                <a href="https://discord.gg/2GBPzayu"><Image className={styles.icon} src="/icons/discord.svg" alt="Discord" height={40} width={40} /></a>
+                {/* <a href="https://discord.gg/2GBPzayu"><Image className={styles.icon} src="/icons/discord.svg" alt="Discord" height={40} width={40} /></a> */}
             </div>
         </footer>
     )
