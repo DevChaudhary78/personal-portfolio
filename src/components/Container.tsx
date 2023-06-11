161431a01bc080db27302da9cb45be1702e4a314
Dev Chaudhary <devchaudhary@Devs-MacBook-Air.local>
2023-06-11 03:26:18 -0400

    Applied Arrows Functionality

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,22 +2,25 @@ import Image from "next/image"
 
 import styles from "./styles/Card.module.css";
 import {cards, Card} from "../assets/cards"
+import { useState } from "react";
 
 export default function Container() {
+    var [currentIdx, setCurrentIdx] = useState(0);
     return (
         <div className={styles.card_container}>
-            <a href="#"><Image className={styles.arrows} src="/imgs/arrow-left.png" alt="This is just a left arrow" width={40} height={40} /></a>
-            {
-                cards.map((card: Card) => (
-                    <CardContainer key={`${card.title}-${card.emoji}`}
-                        backgroundColor={card.backgroundColor}
-                        title={card.title}
-                        emoji={card.emoji}
-                        text={card.text}
-                    />
-                ))
-            }
-            <a href="#" ><Image className={styles.arrows} src="/imgs/arrow-right.png" alt="This is just a right arrow" width={40} height={40} /></a>
+            <button className={styles.arrow_container} onClick={() => {
+                if(currentIdx > 0) {
+                    setCurrentIdx(currentIdx-1)
+                }
+            }}><Image className={styles.arrows} src="/imgs/arrow-left.png" alt="This is just a left arrow" width={40} height={40} /></button>
+
+            <CardContainer key={`${cards[currentIdx].title}-$${cards[currentIdx].emoji}`} backgroundColor={cards[currentIdx].backgroundColor} title={cards[currentIdx].title} emoji={cards[currentIdx].emoji} text={cards[currentIdx].text} />
+            
+            <button className={styles.arrow_container} onClick={() => {
+                if(currentIdx < cards.length - 1) {
+                    setCurrentIdx(currentIdx+1)
+                }
+            }}><Image className={styles.arrows} src="/imgs/arrow-right.png" alt="This is just a right arrow" width={40} height={40} /></button>
         </div>
     );
 }
