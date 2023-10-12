fe45e13a2ba2ba3e04f5a552db986d4517a57a72
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2023-10-12 18:20:42 -0400

    fixed the card size

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,51 +5,56 @@ import {cards} from "../assets/cards"
 import { useState } from "react";
 
 export default function Container() {
-    var [mainIdx, setMainIdx] = useState(0);
-    var [secondIdx, setSecondIdx] = useState(mainIdx+1)
-    return (
-        <div className={styles.card_container}>
-            <button className={styles.arrow_container} onClick={() => {
-                if(mainIdx > 0) {
-                    setMainIdx(mainIdx-1)
-                } else {
-                    setMainIdx(cards.length-1)
-                }
+  var [mainIdx, setMainIdx] = useState(0);
+  var [secondIdx, setSecondIdx] = useState(mainIdx+1)
 
-                if(secondIdx > 0) {
-                    setSecondIdx(secondIdx-1)
-                } else {
-                    setSecondIdx(cards.length-1)
-                }
-            }}><Image className={styles.arrows} src="/imgs/arrow-left-solid.svg" alt="This is just a left arrow" width={40} height={40} /></button>
+  const smallerCardStyle = {
+    height: '250px'
+  }
 
-            <CardContainer key={`${cards[mainIdx].title}-$${cards[mainIdx].emoji}`} backgroundColor={cards[mainIdx].backgroundColor} title={cards[mainIdx].title} emoji={cards[mainIdx].emoji} text={cards[mainIdx].text}  />
-            
-            <CardContainer key={`${cards[secondIdx].title}-$${cards[secondIdx].emoji}`} backgroundColor={cards[secondIdx].backgroundColor} title={cards[secondIdx].title} emoji={cards[secondIdx].emoji} text={cards[secondIdx].text} />
+  return (
+    <div className={styles.card_container}>
+      <button className={styles.arrow_container} onClick={() => {
+        if(mainIdx > 0) {
+          setMainIdx(mainIdx-1)
+        } else {
+          setMainIdx(cards.length-1)
+        }
 
-            <button className={styles.arrow_container} onClick={() => {
-                if(mainIdx < cards.length - 1) {
-                    setMainIdx(mainIdx+1)
-                } else {
-                    setMainIdx(0)
-                }
+        if(secondIdx > 0) {
+          setSecondIdx(secondIdx-1)
+        } else {
+          setSecondIdx(cards.length-1)
+        }
+      }}><Image className={styles.arrows} src="/imgs/arrow-left-solid.svg" alt="This is just a left arrow" width={40} height={40} /></button>
 
-                if(secondIdx < cards.length - 1) {
-                    setSecondIdx(secondIdx+1)
-                } else {
-                    setSecondIdx(0)
-                }
+      <CardContainer key={`${cards[mainIdx].title}-$${cards[mainIdx].emoji}`} backgroundColor={cards[mainIdx].backgroundColor} title={cards[mainIdx].title} emoji={cards[mainIdx].emoji} text={cards[mainIdx].text}  />
 
-            }}><Image className={styles.arrows} src="/imgs/arrow-right-solid.svg" alt="This is just a right arrow" width={40} height={40} /></button>
-        </div>
-    );
+      <CardContainer key={`${cards[secondIdx].title}-$${cards[secondIdx].emoji}`} backgroundColor={cards[secondIdx].backgroundColor} title={cards[secondIdx].title} emoji={cards[secondIdx].emoji} text={cards[secondIdx].text} style={smallerCardStyle}/>
+
+      <button className={styles.arrow_container} onClick={() => {
+        if(mainIdx < cards.length - 1) {
+          setMainIdx(mainIdx+1)
+        } else {
+          setMainIdx(0)
+        }
+
+        if(secondIdx < cards.length - 1) {
+          setSecondIdx(secondIdx+1)
+        } else {
+          setSecondIdx(0)
+        }
+
+      }}><Image className={styles.arrows} src="/imgs/arrow-right-solid.svg" alt="This is just a right arrow" width={40} height={40} /></button>
+    </div>
+  );
 }
 
-function CardContainer({ backgroundColor, title, emoji, text}: {backgroundColor: string, title: string, emoji: string, text: string,}) {
-    return(
-        <div className={styles.card} style={{ backgroundColor }}>
-            <h2 className={styles.card_title}>{title} {emoji}</h2>
-            <p className={styles.card_text}>{text}</p>
-        </div>
-    )
+function CardContainer({ backgroundColor, title, emoji, text, style}: {backgroundColor: string, title: string, emoji: string, text: string, style?: React.CSSProperties}) {
+  return(
+    <div className={styles.card} style={{ backgroundColor, ...style }}>
+      <h2 className={styles.card_title}>{title} {emoji}</h2>
+      <p className={styles.card_text}>{text}</p>
+    </div>
+  )
 }
